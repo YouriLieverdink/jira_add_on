35abda7d051f5eefce271c224e9479ada6255ba4
Youri Lieverdink <89806453+YouriLieverdink@users.noreply.github.com>
2024-07-08 10:20:08 +0200

    feat(#9): add writeln on success

--- lib/jira_add_on.dart
+++ lib/jira_add_on.dart
@@ -1,5 +1,6 @@
 export './src/config.dart';
 export './src/domain/domain.dart';
+export './src/extensions/extensions.dart';
 export './src/models/models.dart';
 export './src/services/jira.dart';
 export './src/utils/utils.dart';
--- lib/src/models/worklog.dart
+++ lib/src/models/worklog.dart
@@ -1,3 +1,33 @@
+import 'package:deep_pick/deep_pick.dart';
+
+class Worklog {
+  final String id;
+  final String timeSpent;
+  final String? comment;
+
+  const Worklog({
+    required this.id,
+    required this.timeSpent,
+    this.comment,
+  });
+
+  factory Worklog.fromJson(Map<String, dynamic> json) {
+    return Worklog(
+      id: pick(json, 'id').asStringOrThrow(),
+      timeSpent: pick(json, 'timeSpent').asStringOrThrow(),
+      comment: pick(json, 'comment').asStringOrNull(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'timeSpent': timeSpent,
+      'comment': comment,
+    };
+  }
+}
+
 class WorklogForm {
   final String timeSpent;
   final String? comment;
